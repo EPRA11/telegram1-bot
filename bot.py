883aa518261dbde277b52dad8c2d80d877da5aaf
EPRA11 <epra74851@gmail.com>
2026-03-14 16:04:42 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ import yt_dlp
 from telegram import Update
 from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes, CommandHandler
 
-TOKEN = os.environ["8644900793:AAHWmsD6ZdIWDYl8hi8uQS7wYJxxIrHVWOw"]
+TOKEN = os.environ["BOT_TOKEN"]
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
